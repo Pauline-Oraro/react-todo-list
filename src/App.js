4f197dfe922e314b9250097cbd297c676d5a5ac6
Pauline-Oraro <hiltrapauline@gmail.com>
2023-06-16 15:40:15 +0300

    React-todo-list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [text, setText] = useState("");
+  const [task, setTask] = useState([]);
+  const [editButton, setEditButton] = useState("edit");
+
+  
+  let inputChange = (event) =>{
+    setText(event.target.value)
+  };
+
+  
+  let addTasks = () =>{
+    setTask([...task, text]);
+  };
+
+  
+  let deleteTask = (id) =>{
+    let fltr = task.filter((val, indx) => {
+      return indx !== id;
+    });
+    console.log(fltr);
+    setTask(fltr)
+  };
+
+  let editFunction = (items,index) =>{
+    if(editButton === "edit"){
+      setEditButton("update")
+      setText(items)
+    } else {
+      setEditButton("edit");
+      task.splice(index,1,text);
+    setText("");
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>TODO LIST &#128221;</h1>
+      <div>
+        <input
+        placeholder='enter your task'
+        value={text}
+        onChange = {inputChange}
+        />
+        <button onClick={addTasks}>Add</button>
+      </div>
+      <div>
+        {task.map((items, index) => {
+          return (
+            <div>
+              <p key={index}>
+                {items}{""}
+                <button onClick={() =>{editFunction(items,index)}}>{editButton}</button>
+                <button onClick={() => {deleteTask(index)}}>Delete</button>
+              </p>
+            </div>
+          )
+        })}
+      </div>
     </div>
   );
 }
